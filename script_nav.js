d5b71c1bb331a7fee8c24a28f60e34ad308a5748
ElenaGir <elena.girard@gmail.com>
2022-04-27 18:44:35 +0200

    menu accordeon

--- script_nav.js
+++ script_nav.js
@@ -1,37 +1,47 @@
 //animation gsap bulles barre de navigation
 const navdots = document.querySelector('#burger-checkbox');
 let b1 = gsap.to('#bulle1', {
-  y: -30, 
-  x: 25,
+  y: -10, 
+  x: 10,
   duration: .5, 
   ease: 'none',
   paused: true});
+
+ let b2 = gsap.to('#bulle2', {
+  y: 15, 
+  x: -15,
+  duration: .5, 
+  ease: 'none',
+  paused: true});
+
 let b3 = gsap.to('#bulle3', {
-  y: -60, 
-  x: -25, 
+  y: -10, 
+  x: -15, 
   duration: .5, 
   ease: 'none',
   paused: true});
 let b4 = gsap.to('#bulle4', {
-  y: -25, 
-  x: 25,
+  y: 15, 
+  x: 10,
   duration: .5, 
   ease: 'none',
   paused: true});
 let b5 = gsap.to('#bulle5', {
-  y: -60, 
-  x: -25, 
+  y: -10, 
+  x: 10, 
   duration: .5, 
   ease: 'none',
   paused: true});
 let b6 = gsap.to('#bulle6', {
-  y: -60, 
+  y: 15,
+  x: -15, 
   duration: .5, 
   ease: 'none',
   paused: true});
 
 navdots.addEventListener('click', () => {
   b1.play();
+  b2.play();
   b3.play();
   b4.play();
   b5.play();
@@ -39,6 +49,7 @@ navdots.addEventListener('click', () => {
   
   if(b1.progress() == 1 && b3.progress() == 1){
      b1.reverse();
+     b2.reverse();
      b3.reverse();
      b4.reverse();
      b5.reverse();
@@ -46,3 +57,22 @@ navdots.addEventListener('click', () => {
      }
 }) 
 
+// menu accordeon
+let question = document.querySelectorAll(".question");
+
+question.forEach(question => {
+  question.addEventListener("click", event => {
+    const active = document.querySelector(".question.active");
+    if(active && active !== question ) {
+      active.classList.toggle("active");
+      active.nextElementSibling.style.maxHeight = 0;
+    }
+    question.classList.toggle("active");
+    const answer = question.nextElementSibling;
+    if(question.classList.contains("active")){
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    } else {
+      answer.style.maxHeight = 0;
+    }
+  })
+})
